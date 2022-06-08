7d2201a1dc65f5533a4219b8d0ceacab25838f8e
F0X18 <adrian.moreno.dominguez@gmail.com>
2022-06-08 13:57:42 +0200

    more changes

---
+++ HelloWorld/src/ejercicio344.java
@@ -0,0 +1,33 @@
+
+public class ejericio344 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2,b=2,c=1,d=3;
+		
+		if((a+b)>(c+d));
+		System.out.println((a+b)>(c+d));
+		
+		int e=100,f=40,g=40;
+		if((e+f+g)==180);
+		System.out.println((e+f+g)==180);
+		
+		
+		if(a%2==0);
+		System.out.println(a%2==0);
+		
+		//ejercicio pagina 348//
+		int num1=2, num2=1, num3=3;
+		if (num1>num2 && num1>num3) {
+			System.out.println("La variable num1 es el mayor");
+		}
+		else if (num2>num3) {
+			System.out.println("La variable num2 es el mayor");
+		}
+		else {
+			System.out.println("La variable num3 es el mayor");
+		}
+	
+	}
+
+}
