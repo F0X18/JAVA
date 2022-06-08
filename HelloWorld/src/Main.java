b1b959638bb53e5cc3c2d09614783f7a32079bc8
F0X18 <adrian.moreno.dominguez@gmail.com>
2022-06-08 13:06:34 +0200

    cambio carpeta

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,63 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //System.out.println("Hola mundo, nuevo lenguage");
+        //System.out.println("2*3");
+        //System.out.println(2*3);
+        /*
+        //Tipo de variable: int (número sin parte decimal)
+        //Nombre de la variable: a
+        //Valor de la variable: 10
+        */
+        //int a = 10, b=2,c=1;
+        // el valor de la variable puede mostrarse mediante System.out.println
+        //System.out.println(a+b+c);
+        //c=4;
+        //System.out.printf("%d+%d+%d=%d",a+b+c).println();
+        
+
+//1-byte
+//2-long
+//3-char
+//4-boolean
+//5-float
+//6-long
+	byte numeroByte = 5;
+	short numeroShort = 128;
+	int numeroInt = 40000;
+	long numeroLong = 2222222222L;
+	float numeroFloat = 2.3f;
+	double numeroDouble = 2.5;
+	boolean variableBooleana = false;
+	
+	//System.out.println(numeroByte + numeroShort);
+	// 2222262222
+	//System.out.println(numeroInt + numeroLong);
+	// 7.3
+	//System.out.println(numeroByte + numeroFloat);
+	// 40002.3
+	//System.out.println(numeroInt + numeroFloat);
+	// 4.799999952316284
+	//System.out.println(numeroFloat + numeroDouble);
+	
+	int a=2,b=2,c=1,d=3;
+	
+	if((a+b)==(c+d));
+	System.out.println((a+b)==(c+d));
+	
+	int e=100,f=40,g=40;
+	if((e+f+g)==180);
+	System.out.println((e+f+g)==180);
+	
+	
+	if(a%2==0);
+	System.out.println(a%2==0);
+	
+	
+	
+	
+	
+	}
+	
+}
