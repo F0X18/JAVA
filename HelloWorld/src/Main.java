1a876e4501845c1819e51b7046e526650e93ed78
F0X18 <adrian.moreno.dominguez@gmail.com>
2022-06-09 12:50:36 +0200

    proyectos 1,2,3

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -41,23 +41,7 @@ public class Main {
 	// 4.799999952316284
 	//System.out.println(numeroFloat + numeroDouble);
 	
-	int a=2,b=2,c=1,d=3;
-	
-	if((a+b)==(c+d));
-	System.out.println((a+b)==(c+d));
-	
-	int e=100,f=40,g=40;
-	if((e+f+g)==180);
-	System.out.println((e+f+g)==180);
-	
-	
-	if(a%2==0);
-	System.out.println(a%2==0);
-	
-	
-	
-	
-	
+			}
+			
 	}
 	
-}
