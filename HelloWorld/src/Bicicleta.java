a51a3186a4a43b650d4fbbe00858b3a45a420fe5
F0X18 <adrian.moreno.dominguez@gmail.com>
2022-06-13 13:58:36 +0200

    14;00

---
+++ HelloWorld/src/Bicicleta.java
@@ -0,0 +1,11 @@
+
+public class Bicicleta extends Vehiculo {
+	private boolean cambios;
+	
+	public Bicicleta() {
+		super(0.0f,4,0);
+		this.cambios=3;
+	}
+	
+	}
+}
---
+++ HelloWorld/src/Vehiculo.java
@@ -0,0 +1,18 @@
+
+public class Vehiculo {
+	//atributos
+	protected float velocidad;
+	protected int asientos;
+	protected int orientacionRuedas;
+	//constructor
+	public.Vehiculo(float velocidad,int asientos,int orientacionRuedas)
+	
+	//metodos
+   public void parar () {} 
+   public void acelerar( ) {} 
+   public void desacelerar() {} 
+   public void giararIzquierda() {} 
+   public void girarDerecha() {} 
+
+ 
+}
