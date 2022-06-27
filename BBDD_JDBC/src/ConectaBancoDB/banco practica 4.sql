b82b5b8d1d18c4d945a713221ffdf1ba89d04e26
F0X18 <105197972+F0X18@users.noreply.github.com>
2022-06-27 10:08:10 +0200

    Add files via upload

---
+++ BBDD_JDBC/src/ConectaBancoDB/banco practica 4.sql
@@ -0,0 +1,254 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-06-2022 a las 10:07:19
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `banco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `id_gestor` int(11) NOT NULL,
+  `usuario` char(20) NOT NULL,
+  `password` char(64) NOT NULL,
+  `correo` char(50) NOT NULL,
+  `saldo` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `id_gestor`, `usuario`, `password`, `correo`, `saldo`) VALUES
+(1, 1, 'pepito', '627a', 'pepito@correo.com', 2000),
+(2, 1, 'juanito', '44e125', 'juanito@correo.com', 2050),
+(3, 1, 'juanita', '44e15234', 'juanita@correo.es', 2099);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gestor`
+--
+
+CREATE TABLE `gestor` (
+  `id` int(11) NOT NULL,
+  `usuario` char(20) NOT NULL,
+  `password` char(64) NOT NULL,
+  `correo` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `gestor`
+--
+
+INSERT INTO `gestor` (`id`, `usuario`, `password`, `correo`) VALUES
+(1, 'gestor1', 'asdcf1', 'gestor1@correo.com'),
+(2, 'gestor2', 'gestor2', 'gestor2@correo.com'),
+(3, 'gestor3', 'gestor3', 'gestor3@gmail.com'),
+(4, 'gestor4', 'gestor4', 'gestor4@correo.com'),
+(5, 'gestor5', 'gestor5', 'gestor5@correo.com'),
+(11, 'gestor6', 'gestor6', 'gestor6@correo.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensaje`
+--
+
+CREATE TABLE `mensaje` (
+  `id` int(11) NOT NULL,
+  `id_origen` int(11) NOT NULL,
+  `id_destino` int(11) NOT NULL,
+  `texto` char(140) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `mensaje`
+--
+
+INSERT INTO `mensaje` (`id`, `id_origen`, `id_destino`, `texto`, `fecha`) VALUES
+(1, 2, 2, 'hola', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamo`
+--
+
+CREATE TABLE `prestamo` (
+  `id` int(11) NOT NULL,
+  `nombre` char(20) NOT NULL,
+  `comision` double NOT NULL,
+  `meses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamo_relacion`
+--
+
+CREATE TABLE `prestamo_relacion` (
+  `id_prestamo` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transferencia`
+--
+
+CREATE TABLE `transferencia` (
+  `id` int(11) NOT NULL,
+  `id_ordenante` int(11) NOT NULL,
+  `id_beneficiario` int(11) NOT NULL,
+  `importe` double NOT NULL,
+  `concepto` varchar(40) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `transferencia`
+--
+
+INSERT INTO `transferencia` (`id`, `id_ordenante`, `id_beneficiario`, `importe`, `concepto`, `fecha`) VALUES
+(1, 2, 2, 200, 'cash', '0000-00-00 00:00:00');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_gestor` (`id_gestor`);
+
+--
+-- Indices de la tabla `gestor`
+--
+ALTER TABLE `gestor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `mensaje`
+--
+ALTER TABLE `mensaje`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_destino` (`id_destino`),
+  ADD KEY `id_origen` (`id_origen`);
+
+--
+-- Indices de la tabla `prestamo`
+--
+ALTER TABLE `prestamo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `prestamo_relacion`
+--
+ALTER TABLE `prestamo_relacion`
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_prestamo` (`id_prestamo`);
+
+--
+-- Indices de la tabla `transferencia`
+--
+ALTER TABLE `transferencia`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_ordenante` (`id_ordenante`),
+  ADD KEY `id_beneficiario` (`id_beneficiario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `gestor`
+--
+ALTER TABLE `gestor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `mensaje`
+--
+ALTER TABLE `mensaje`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamo`
+--
+ALTER TABLE `prestamo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `transferencia`
+--
+ALTER TABLE `transferencia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_gestor`) REFERENCES `gestor` (`id`);
+
+--
+-- Filtros para la tabla `mensaje`
+--
+ALTER TABLE `mensaje`
+  ADD CONSTRAINT `mensaje_ibfk_1` FOREIGN KEY (`id_destino`) REFERENCES `gestor` (`id`),
+  ADD CONSTRAINT `mensaje_ibfk_2` FOREIGN KEY (`id_origen`) REFERENCES `gestor` (`id`);
+
+--
+-- Filtros para la tabla `prestamo_relacion`
+--
+ALTER TABLE `prestamo_relacion`
+  ADD CONSTRAINT `prestamo_relacion_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `prestamo_relacion_ibfk_2` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamo` (`id`);
+
+--
+-- Filtros para la tabla `transferencia`
+--
+ALTER TABLE `transferencia`
+  ADD CONSTRAINT `transferencia_ibfk_1` FOREIGN KEY (`id_ordenante`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `transferencia_ibfk_2` FOREIGN KEY (`id_beneficiario`) REFERENCES `cliente` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
